ae183ad5df62e3d0ddc43a947440d53a9e9e1299
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-09-28 22:23:18 +0200

    UI Added

--- app/src/main/java/com/seif/errorhandlingcleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/seif/errorhandlingcleanarchitecture/presentation/MainActivity.kt
@@ -3,12 +3,14 @@ package com.seif.errorhandlingcleanarchitecture.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.seif.errorhandlingcleanarchitecture.presentation.ui.theme.ErrorHandlingCleanArchitectureTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,12 +18,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ErrorHandlingCleanArchitectureTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val viewModel = viewModel<MainViewModel>()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 32.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-
+                    TextField(
+                        value = viewModel.email,
+                        onValueChange = viewModel::onEmailChanged,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(
+                        onClick = viewModel::submitEmail,
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text(text = "Submit")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    if (viewModel.error != null) {
+                        Text(text = viewModel.error ?: "")
+                    }
                 }
             }
         }
--- app/src/main/java/com/seif/errorhandlingcleanarchitecture/presentation/MainViewModel.kt
+++ app/src/main/java/com/seif/errorhandlingcleanarchitecture/presentation/MainViewModel.kt
@@ -4,13 +4,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.seif.errorhandlingcleanarchitecture.domain.usecase.SubmitEmailUseCase
+import com.seif.errorhandlingcleanarchitecture.util.Resource
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
-    private var email by mutableStateOf("")
-    private var error by mutableStateOf<String?>(null)
+class MainViewModel(
+    private val submitEmailUseCase: SubmitEmailUseCase = SubmitEmailUseCase()
+) : ViewModel() {
+    var email by mutableStateOf("")
+        private set
+    var error by mutableStateOf<String?>(null)
+        private set
 
-    fun onEmailChanged(email:String) {
+    fun onEmailChanged(email: String) {
         this.email = email
+    }
+
+    fun submitEmail() {
+        viewModelScope.launch {
+            val result = submitEmailUseCase(email)
+            when (result) {
+                is Resource.Success -> {
+
+                }
+                is Resource.Error -> {
+                    error = result.message
+                }
+            }
+        }
     }
 
 }
